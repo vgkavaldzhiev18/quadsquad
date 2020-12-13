d2d7f2041220f0b6d85c85585b6691cf81c7096f
VTStanchev18 <56872381+VTStanchev18@users.noreply.github.com>
2020-12-13 23:58:39 +0200

    Add .cpp file: add initial greeting and start developing main menu

---
+++ bletchleyCode/bletchleyCode/bletchleyCode.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+
+//void initialGreeting()
+//{
+//	cout << "                      █ █ █ █▀▀ █   █▀▀ █▀█ █▄ ▄█ █▀▀  ▀█▀ █▀█" << endl;
+//	cout << "                      ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █ ▀ █ ██▄   █  █▄█" << endl;
+
+//	cout << "█▄▄ █   █▀▀ ▀█▀ █▀▀ █▄█ █   █▀▀ ▀▄▀ █▀▀ █▀█ █▀▄ █▀▀   █▄▄ █▀█ █▀▀ ▄▀▄ █▄▀ █▀▀ █▀█ █▀▀ █" << endl;
+//	cout << "█▄█ █▄▄ ██▄  █  █▄▄ █ █ █▄▄ ██▄  █  █▄▄ █▄█ █▄▀ ██▄   █▄█ █▀▄ ██▄ █▀█ █ █ ██▄ █▀▄ ▄██ ▄" << endl;
+//	cout << "                                      Copyright©" << endl;
+//}
+
+void rules()
+{
+
+}
+
+int enterUserInput()
+{
+	int userInput;
+	cin >> userInput;
+	return userInput;
+}
+
+bool mainMenu()
+{
+	// initialGreeting();
+	cout << "\n|---------MENU----------|\n";
+	cout << "1. Play against AI\n";
+	cout << "2. Play with another player\n";
+	cout << "3. Rules\n";
+	int userInput = enterUserInput();
+	switch (userInput)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3: 
+		// rules();
+		break;
+
+	default: cout << "\n Please enter a valid input!\n"; break;
+	}
+	return true;
+}
+
+int main()
+{
+	
+	do
+	{
+		mainMenu();
+	} while (true);
+}
